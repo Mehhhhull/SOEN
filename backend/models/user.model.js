574f11ee93ea5dcc208b7b4832bf239815507dd4
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-12-14 22:17:07 +0530

    Project setup and DB integration

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import becrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+// Define the user schema
+
+const userSchema=new mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    lowercase:true,
+    minLength:[6,"Email must be at least 6 character long"],
+    maxLength:[50,"Email must not be longer than 50 character long"],
+  },
+  password:{
+    type:String,
+    select:false,
+  }
+})
+
+userSchema.statics.hashPassword=async function(password){
+  return await becrypt.hash(password,10);
+}
+
+userSchema.methods.isValidPassword=async function(password){
+  return await becrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateJWT=function() {
+  return jwt.sign({email: this.email},process.env.JWT_SECRET )
+}
