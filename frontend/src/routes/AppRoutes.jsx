fcaa83357aff291eb88f9931956ae3239ab7385c
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-12-16 21:13:16 +0530

    React Talwind Setup

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/">
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
